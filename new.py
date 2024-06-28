1ae41e6719274e5ddf3ec81e212c28fde9f2bec4
NishanD21 <nishandhan21@gmail.com>
2024-06-28 11:50:09 +0530

    changes

--- new.py
+++ new.py
@@ -2,3 +2,10 @@ print("Hello")
 
 name = input("What is your name?")
 print(f'Hi! {name}')
+
+status = input("How are you?")
+
+if status == "Good":
+    print("Nice to hear")
+elif status == "Bad":
+    print("How Saad")
